b1e6d60a36021b92a7a07f48a57c8e87fb8cd996
soumya-mishra <39845943+soumya-mishra@users.noreply.github.com>
2018-06-03 22:33:09 +0530

    Create Data_preprocessing.py
    contains the explanation and code for data pre-processing  in python

---
+++ Data_preprocessing.py
@@ -0,0 +1,58 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+#Loading Iris datasets
+"""from sklearn import datasets
+iris = datasets.load_iris()
+digits = datasets.load_digits()
+print(digits.data)
+print(digits.target)"""
+
+
+# Taking care of missing data
+#1.Standardizaton
+#2.Minmax scaler
+#3.MaxAbsScaler
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data through OneHotencoder
+#OneHotEncoder means one value will be 1 and others will be Zeros
+# Encoding the Independent Variable
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+#instead of preprocessing we are using Model_selection so that no warning would appear
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+#it is already fitted to training set
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+#if it is a regression problem then we might need to do feature scaling for dependant variable
+
+
