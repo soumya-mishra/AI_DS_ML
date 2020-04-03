5cfa5895728b6bdf2a54c6c4f55608527a10bfe9
Soumya <soumyaiter@gmail.com>
2020-04-03 22:30:48 +0530

    Update Random_Forest.py

--- Random Forest/Random_Forest.py
+++ Random Forest/Random_Forest.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
@@ -26,6 +27,7 @@ y_train = sc_y.fit_transform(y_train)"""
 from sklearn.ensemble import RandomForestRegressor
 regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
 regressor.fit(X, y)
+
 
 # Predicting a new result
 y_pred = regressor.predict(6.5)
